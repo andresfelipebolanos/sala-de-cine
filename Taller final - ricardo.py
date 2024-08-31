9b257698d211de013b33089908a0b43f39ef82e0
unknown <alvrick842@gmail.com>
2024-08-31 10:54:51 -0500

    enviar segundo cambio

--- Taller final - ricardo.py
+++ Taller final - ricardo.py
@@ -1,8 +1,31 @@
 # Lista de películas
 peliculas = [{"titulo": "La La Land", "genero": "Musical", "duracion": 128, "horarios": ["19:00", "21:30"]}, {"titulo": "Interstellar", "genero": "Ciencia ficción", "duracion": 169, "horarios": ["20:00"]}]
 
-# Diccionario de un usuario
+# usuario
 usuario1 = {"nombre": "Juan Pérez", "email": "juanperez@example.com", "historial": ["La La Land"]}
 
 # Matriz para representar una sala de cine (simplificado)
-sala1 = [["Disponible", "Disponible", "Reservado"], ["Disponible", "Disponible", "Disponible"]]
+
+salas = {
+    1: [
+        ["A1", "A2", "A3", "A4"],
+        ["B1", "B2", "B3", "B4"],
+        ["C1", "C2", "C3", "C4"],
+        ["D1", "D2", "D3", "D4"]
+    ],
+    2: [
+        ["A1", "A2", "A3", "A4"],
+        ["B1", "B2", "B3", "B4"],
+        ["C1", "C2", "C3", "C4"],
+        ["D1", "D2", "D3", "D4"]
+    ],
+    3: [
+        ["A1", "A2", "A3", "A4"],
+        ["B1", "B2", "B3", "B4"],
+        ["C1", "C2", "C3", "C4"],
+        ["D1", "D2", "D3", "D4"]
+        ]
+}
+
+
+
