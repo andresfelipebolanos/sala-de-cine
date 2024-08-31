1c236f70e1eba2274a768050919053319ec6da43
unknown <alvrick842@gmail.com>
2024-08-31 08:40:40 -0500

    enviar primer cambio

---
+++ Taller final - ricardo.py
@@ -0,0 +1,8 @@
+# Lista de películas
+peliculas = [{"titulo": "La La Land", "genero": "Musical", "duracion": 128, "horarios": ["19:00", "21:30"]}, {"titulo": "Interstellar", "genero": "Ciencia ficción", "duracion": 169, "horarios": ["20:00"]}]
+
+# Diccionario de un usuario
+usuario1 = {"nombre": "Juan Pérez", "email": "juanperez@example.com", "historial": ["La La Land"]}
+
+# Matriz para representar una sala de cine (simplificado)
+sala1 = [["Disponible", "Disponible", "Reservado"], ["Disponible", "Disponible", "Disponible"]]
