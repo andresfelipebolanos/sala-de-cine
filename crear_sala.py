284f9e3f5c97f9bb003e7e445ec4980aba5222a0
andresfelipebolanos <andresto123@gmail.com>
2024-08-31 10:20:33 -0500

    modificacion funcion crear sala

--- crear_sala.py
+++ crear_sala.py
@@ -1,12 +1,4 @@
-#defino la matriz principal para fines de prueba, pues esta habita en el main del programa
-matriz_principal=[]
-fil=(100)
-col=(100)
-for i in range (fil):
-        matriz_principal.append([])
-        for j in range (col):
-            matriz_principal[i].append(0)
-#crear metodo para crear sala modificando la matriz principal
+
 def crear_sala():
     fila = int(input('Ingrese numero de filas: '))
     colum= int(input('ingresa nuemro de columnas: '))
