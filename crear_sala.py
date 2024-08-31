78c43fb6978c07f64c96e2700cdd7d38d6c789d2
andresfelipebolanos <andresto123@gmail.com>
2024-08-30 20:54:01 -0500

    correcion1

--- crear_sala.py
+++ crear_sala.py
@@ -1,38 +1,25 @@
+#defino la matriz principal para fines de prueba, pues esta habita en el main del programa
 matriz_principal=[]
-fila=(100)
-colum=(100)
-for i in range (fila):
+fil=(100)
+col=(100)
+for i in range (fil):
         matriz_principal.append([])
-        for j in range (colum):
-            matriz_principal[i].append(None)
-#crear metodo para crear sala recibiendo la matriz principal del programa
+        for j in range (col):
+            matriz_principal[i].append(0)
+#crear metodo para crear sala modificando la matriz principal
 def crear_sala():
-    def llenar_sala():
-        fila = int(input('Ingrese numero de filas: '))
-        colum= int (input('ingresa nuemro de columnas: '))
-        acu=0
-        sala=[]
-        for i in range (fila):
-            sala.append([])
-            for j in range (colum):
-                sala[i].append(0)
-           
-           
-        for i in range (fila):
+    fila = int(input('Ingrese numero de filas: '))
+    colum= int(input('ingresa nuemro de columnas: '))
+    acu=0
+    sala=[]
+    for i in range (fila):
+        sala.append([])
+        for j in range (colum):
+            sala[i].append(0)
+    for i in range (fila):
             for j in range(colum):
                 acu+=1 
-                sala[i][j]=acu 
-                matriz_principal=sala
-        print (sala)
-    llenar_sala()
-
-
-opc=str(input('crear sala? s/n'))
-if opc == 's':
-    crear_sala()
-elif opc=='n':
-    main()
-else:
-    print('seleccione opcion valida')
-
+                sala[i][j]=acu
+    print (sala)
+crear_sala()
 
