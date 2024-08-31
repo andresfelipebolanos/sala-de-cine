a0069bb676950f88230e5b81f1a034f2ca732e71
unknown <alvrick842@gmail.com>
2024-08-31 11:42:33 -0500

    enviar tercer cambio

--- Taller final - ricardo.py
+++ Taller final - ricardo.py
@@ -1,31 +1,9 @@
-# Lista de películas
-peliculas = [{"titulo": "La La Land", "genero": "Musical", "duracion": 128, "horarios": ["19:00", "21:30"]}, {"titulo": "Interstellar", "genero": "Ciencia ficción", "duracion": 169, "horarios": ["20:00"]}]
 
-# usuario
-usuario1 = {"nombre": "Juan Pérez", "email": "juanperez@example.com", "historial": ["La La Land"]}
-
-# Matriz para representar una sala de cine (simplificado)
-
-salas = {
-    1: [
-        ["A1", "A2", "A3", "A4"],
-        ["B1", "B2", "B3", "B4"],
-        ["C1", "C2", "C3", "C4"],
-        ["D1", "D2", "D3", "D4"]
-    ],
-    2: [
-        ["A1", "A2", "A3", "A4"],
-        ["B1", "B2", "B3", "B4"],
-        ["C1", "C2", "C3", "C4"],
-        ["D1", "D2", "D3", "D4"]
-    ],
-    3: [
-        ["A1", "A2", "A3", "A4"],
-        ["B1", "B2", "B3", "B4"],
-        ["C1", "C2", "C3", "C4"],
-        ["D1", "D2", "D3", "D4"]
-        ]
-}
-
-
-
+def ver_sala():  
+  for f in sala :
+      print("", end="")
+      for c in f:
+         print("{:4}".format(c), end='')
+      print('')
+      
+print()
