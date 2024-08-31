22b0e669399ba9b580f7589b39a0912ef071d631
unknown <alvrick842@gmail.com>
2024-08-31 16:07:57 -0500

    enviar tercer cambio

--- Taller final - ricardo.py
+++ Taller final - ricardo.py
@@ -1,4 +1,18 @@
 
+salas = {
+    1: [
+        ["1", "2", "3"],
+        ["4", "5", "6"],
+        ["7", "8", "9"],
+    ],
+    2: [
+        ["1", "2", "3"],
+        ["4", "5", "6"],
+        ["7", "8", "9"],
+    ]
+}
+
+
 def ver_sala():  
   for f in sala :
       print("", end="")
@@ -7,3 +21,22 @@ def ver_sala():
       print('')
       
 print()
+
+
+def crear_sala():
+    try:
+        filas = int(input("Ingrese el número de filas de la sala: "))
+        columnas = int(input("Ingrese el número de columnas de la sala: "))
+        
+        nueva_sala = []
+        n_asiento = 1  
+        for _ in range(filas):
+            fila = []
+            for _ in range(columnas):
+                fila.append(n_asiento)
+                n_asiento += 1
+            nueva_sala.append(fila)
+
+        numero_sala = len(salas) + 1
+        new_func(nueva_sala, numero_sala)
+
